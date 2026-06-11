aebecc7b3bfdf6b185784c262477ca43081173b9
danialza <danial.za@outlook.com>
2026-06-11 03:20:16 +0100

    build: PreferenceKey.defaultValue must be 'let' for Swift 6 concurrency

--- apps/mac/Sources/AtelierMac/SidebarAwareLeadingPadding.swift
+++ apps/mac/Sources/AtelierMac/SidebarAwareLeadingPadding.swift
@@ -37,7 +37,7 @@ struct SidebarAwareLeadingPadding: ViewModifier {
 }
 
 private struct GlobalXKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
+    static let defaultValue: CGFloat = 0
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
         value = nextValue()
     }
